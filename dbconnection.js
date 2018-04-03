bcb4b27f81629c47e980a3eea7bf74f366ff1132
Giang <giangth9723@gmail.com>
2018-04-03 20:10:28 +0700

    hello

--- dbconnection.js
+++ dbconnection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-	password:'',
-	database: 'doctorhow'
+	host:'sql12.freemysqlhosting.net',
+	user:'sql12230427',
+	password:'yg2s91JkUX',
+	database: 'sql12230427'
 });
 module.exports=connection;
