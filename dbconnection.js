aaf63f75a68067a840707f9d01ad673b9cff5776
Giang <giangth9723@gmail.com>
2018-04-11 22:20:58 +0700

    update 11/4/2018

--- dbconnection.js
+++ dbconnection.js
@@ -1,15 +1,15 @@
 var mysql = require("mysql");
-// var connection = mysql.createConnection({
-// 	host:'sql12.freemysqlhosting.net',
-// 	user:'sql12230427',
-// 	password:'yg2s91JkUX',
-// 	database: 'sql12230427'
-// });
-// module.exports=connection;
 var connection = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-	password:'',
-	database: 'doctorhow'
+	host:'sql12.freemysqlhosting.net',
+	user:'sql12232037',
+	password:'EtYXDgmaph',
+	database: 'sql12232037'
 });
 module.exports=connection;
+// var connection = mysql.createConnection({
+// 	host:'localhost',
+// 	user:'root',
+// 	password:'',
+// 	database: 'doctorhow'
+// });
+// module.exports=connection;
