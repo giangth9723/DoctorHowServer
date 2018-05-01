6c5c0ddaaa0ca16eb5a8108e7d4a2efeb7f4895c
Giang <giangth9723@gmail.com>
2018-05-01 16:35:05 +0700

    update 01_05_2018

--- dbconnection.js
+++ dbconnection.js
@@ -1,13 +1,13 @@
 var mysql = require("mysql");
-var pool = mysql.createPool({
-	connectionLimit : 10,
-	host:'us-cdbr-iron-east-05.cleardb.net',
-	user:'b03d48412a2911',
-	password:'3fc638fc',
-	database: 'heroku_10443ed5968f512',
-	dateStrings : true
-});
-module.exports = pool;
+// var connection = mysql.createPool({
+// 	connectionLimit : 10,
+// 	host:'us-cdbr-iron-east-05.cleardb.net',
+// 	user:'b03d48412a2911',
+// 	password:'3fc638fc',
+// 	database: 'heroku_10443ed5968f512',
+// 	dateStrings : true
+// });
+// module.exports = connection;
 // var connection = mysql.createConnection({
 // 	host:'localhost',
 // 	user:'root',
@@ -16,3 +16,11 @@ module.exports = pool;
 // 	dateStrings : true
 // });
 // module.exports=connection;
+var connection = mysql.createConnection({
+	host:'sql12.freemysqlhosting.net',
+	user:'sql12235712',
+	password:'VSaK75UxLG',
+	database: 'sql12235712',
+	dateStrings : true
+});
+module.exports = connection;
